51f19b90dbac998b1e373975ae8202775c8cef56
Naomi T. S <tsnaomi@uw.edu>
2016-07-05 19:39:39 -0700

    increase session lifetime

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 from datetime import timedelta
 
-PERMANENT_SESSION_LIFETIME = timedelta(hours=5)
+PERMANENT_SESSION_LIFETIME = timedelta(hours=10)
 SECRET_KEY = os.getenv('SECRET_KEY', '31415926535')
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql:///metric-gold')  # noqa
 SQLALCHEMY_TRACK_MODIFICATIONS = False
