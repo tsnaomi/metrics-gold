6575e411d26689dd0e11a6783f2b378c30d9556c
Naomi T. S <tsnaomi@uw.edu>
2017-01-25 23:04:13 -0800

    add note

--- worker.py
+++ worker.py
@@ -3,6 +3,9 @@ import redis
 
 from rq import Worker, Queue, Connection
 
+# redis-server
+# python worker.py
+
 listen = ['default', ]
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
