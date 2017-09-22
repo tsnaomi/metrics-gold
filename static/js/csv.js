1af59be08485fbe68ccf1568b212872f0bf9549f
Naomi Shapiro <tsnaomi@uw.edu>
2017-09-22 11:45:09 -0700

    add js logs

--- static/js/csv.js
+++ static/js/csv.js
@@ -5,19 +5,23 @@
         var file = $text.attr('id');
         var csrf_token = $text.attr('name');
         var csv_view = '/csv/' + file;
+        console.log('Downloading...');
 
         // get the csv via ajax
         $.post(csv_view, { _csrf_token: csrf_token }, function() {
             // yea!
+            console.log('Success!');
             window.location.href = '/static/csv/' + file + '.csv';
             $text.html('The csv <strong>' + file + '</strong> has successfully downloaded.');
             $title.text('Download complete!');
         }).fail(function(jqXHR, textStatus, errorThrown) {
             // nay!
+            console.log('Failure!');
             alert('Sorry, something went awry!\n\nError: ' + (errorThrown || 'Unknown'));
             $text.html('The csv <strong>' + file + '</strong> has failed to download.');
             $title.text('Download failed.');
         }).always(function() {
+            console.log('Removing spinner.');
             // remove loading overlay
             $('.loading').remove();
         });
