4a89c15c42e105aecc44c8c58cc4f74386faacac
Naomi Shapiro <tsnaomi@uw.edu>
2017-09-22 11:36:06 -0700

    revise page titles

--- static/js/csv.js
+++ static/js/csv.js
@@ -1,19 +1,22 @@
 (function() {
     function generage_csv() {
         var $text = $('.text');
-        var title = $text.attr('id');
+        var $title = $('title');
+        var file = $text.attr('id');
         var csrf_token = $text.attr('name');
-        var csv_view = '/csv/' + title;
+        var csv_view = '/csv/' + file;
 
         // get the csv via ajax
         $.post(csv_view, { _csrf_token: csrf_token }, function() {
             // yea!
-            window.location.href = '/static/csv/' + title + '.csv';
-            $text.html('The csv <strong>' + title + '</strong> has successfully download.')
+            window.location.href = '/static/csv/' + file + '.csv';
+            $text.html('The csv <strong>' + file + '</strong> has successfully download.');
+            $title.text('Download complete!');
         }).fail(function(jqXHR, textStatus, errorThrown) {
             // nay!
             alert('Sorry, something went awry!\n\nError: ' + (errorThrown || 'Unknown'));
-            $text.html('The csv <strong>' + title + '</strong> has failed to download.')
+            $text.html('The csv <strong>' + file + '</strong> has failed to download.');
+            $title.text('Download failed.');
         }).always(function() {
             // remove loading overlay
             $('.loading').remove();
