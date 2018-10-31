7d315539c0f49055a282a5d53743c4b4416d3e89
Naomi Shapiro <tsnaomi@uw.edu>
2018-10-30 21:26:21 -0700

    update transition.py

--- transition.py
+++ transition.py
@@ -2,8 +2,11 @@
 
 from app import db, get_status, Sentence, User
 
+# [classroom bc8d584]
 # python app.py db upgrade c2936ba298a0
 # python transition.py
+# [current]
+# python app.py db upgrade 8752f86d1e89
 
 for user in User.query.all():
 
