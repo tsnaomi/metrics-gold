7aad4bfae6b4a37552768061ffaf8cc003bb9b11
Naomi Shapiro <tsnaomi@uw.edu>
2019-10-07 20:21:28 -0700

    add (new) videos for Bush 1989 and Clinton 1993/1997

--- aeneas-play.py
+++ aeneas-play.py
@@ -12,7 +12,10 @@ from app import db, get_doc
 
 D = Downloader()
 
-# missing 1937-Roosevelt2, 1989-Bush, and 1997-Clinton2
+# automatic alignment ---------------------------------------------------------
+
+# This dictionary matches inaugural addresses to YouTube recordings.
+# (TODO: It is currently missing a recording for 1937-Roosevelt2.)
 text_audio_pairs = {
     # text added initially: "President Hoover , Mr. Chief Justice , my friends
     # , this is a day of national consecration , and "
@@ -39,18 +42,84 @@ text_audio_pairs = {
     '1977-Carter': 'tMJ8yDSbhhU',
     '1981-Reagan1': 'hpPt7xGx4Xo',
     '1985-Reagan2': 'OBthkDPFBvg',
-    '1993-Clinton1': '7LdagSzK2VI',
     '2001-Bush1': 'rXzgMdj5urs',
     '2005-Bush2': 'ceSJLivxk2k',
     '2009-Obama1': 'n_94YT46yFg',
 
     # fix first two alignments
     '2013-Obama2': 'g5bIT5JJX5U',
+
+    # OK :)
+    '1989-Bush': 'zMmrNcdmdVY',
+    '1993-Clinton1': 'Qszv668rN20',
+    '1997-Clinton2': 'Tu33kA83Rfo',
     }
 
+
+def align(address_id, youtube_id=None):
+    '''Use aeneas to align the given address and YouTube video.'''
+    if not youtube_id:
+        youtube_id = text_audio_pairs[address_id]
+
+    # download YouTube video
+    print('Downloading YouTube video...')
+    print(D.audio_from_youtube(
+        'http://www.youtube.com/watch?v=' + youtube_id,
+        output_file_path=u'alignment/audio/' + address_id + '.webm',
+        # preferred_format=u'webm'
+        ))
+
+    # designate text, audio, and syncmap files
+    print('Designating filepaths...')
+    text = os.path.abspath(u'alignment/text/' + address_id + '.txt')
+    audio = os.path.abspath(u'alignment/audio/' + address_id + '.webm')
+    syncmap = os.path.abspath(u'alignment/syncmaps/' + address_id + '.json')
+
+    # align text to audio
+    print('Align speech to text...')
+    config = u'task_language=eng|is_text_type=plain|os_task_file_format=json'
+    task = Task(config_string=config)
+    task.text_file_path_absolute = text
+    task.audio_file_path_absolute = audio
+    task.sync_map_file_path_absolute = syncmap
+    ExecuteTask(task).execute()
+    task.output_sync_map_file()
+
+
+def update(address_id, youtube_id=None):
+    '''Update the database with the aeneas timestamps for the given address.'''
+    if not youtube_id:
+        youtube_id = text_audio_pairs[address_id]
+
+    doc = get_doc(title=address_id)
+    doc.youtube_id = youtube_id
+
+    syncmap = os.path.abspath(u'alignment/syncmaps/' + address_id + '.json')
+    syncmap = json.load(open(syncmap))['fragments']
+
+    for sent, sync in zip(doc.sentences, syncmap):
+        sent.youtube_id = youtube_id
+        sent.aeneas_start = float(sync['begin'])
+        sent.aeneas_end = float(sync['end'])
+
+        # to reset any previous forced alignments
+        sent.manual_start = None
+        sent.manual_end = None
+
+        db.session.add(sent)
+
+    db.session.add(doc)
+    db.session.commit()
+
+
+# revisions -------------------------------------------------------------------
+
+# This dictionary provides forced/manual alignments for specified sentences in
+# misaligned inaugural addresses.
+# (TODO: The alignments for 1997-Clinton2 have not been cleaned yet.)
 alignment_fixes = {
     '1933-Roosevelt1': {
-        # index: (start, end)
+        # sentence index: (start time, end time)
         97: (1047.720, 1056),
         98: (1056, 1063),
         99: (1063, 1072),
@@ -63,7 +132,6 @@ alignment_fixes = {
         106: (1116, 1123),
         107: (1123, 1147.720)
         },
-    '1941-Roosevelt3': {},
     '1945-Roosevelt4': {
         1: (0, 0),
         2: (0, 11.520),
@@ -72,8 +140,6 @@ alignment_fixes = {
         1: (0, 0),
         2: (0, 8.840),
         },
-    '1953-Eisenhower1': {},
-    '1957-Eisenhower2': {},
     '1961-Kennedy': {
         5: (47.840, 62),
         6: (62, 66),
@@ -153,13 +219,27 @@ alignment_fixes = {
         92: (828, 836),
         93: (835, 838.800),
         },
-    '1993-Clinton1': {},
+    '1993-Clinton1': {
+        # ... ends late
+        9: (109, 120),
+        36: (310.5, 312.5),
+        38: (329, 336.5),
+        40: (356, 360),
+        50: (415, 419),
+        54: (446, 454.5),
+        59: (504, 506.5),
+        62: (514, 522),
+        65: (544, 549),
+        69: (573.5, 575),
+        72: (587, 593),
+        93: (719.5, 722),
+        109: (831, 838.5),
+        110: (838.5, 841.5),
+    },
     '2001-Bush1': {
         1: (13, 15),
         2: (15, 38.320),
         },
-    '2005-Bush2': {},
-    '2009-Obama1': {},
     '2013-Obama2': {
         1: (3, 10),
         2: (20, 23.440),
@@ -170,62 +250,102 @@ alignment_fixes = {
     }
 
 
-def get_alignments():
-    config = u'task_language=eng|is_text_type=plain|os_task_file_format=json'
+def fix(address_id):
+    '''Fix the alignments for the given address based on alignment_fixes.'''
+    doc = get_doc(title=address_id)
 
-    for address, youtube_id in text_audio_pairs.iteritems():
-
-        # download youtube video
-        print D.audio_from_youtube(
-            'https://youtu.be/' + youtube_id,
-            output_file_path=u'alignment/audio/' + address + '.webm',
-            preferred_format=u'webm'
-            )
-
-        # designate text, audio, and syncmap files
-        text = os.path.abspath(u'alignment/text/' + address + '.txt')
-        audio = os.path.abspath(u'alignment/audio/' + address + '.webm')
-        syncmap = os.path.abspath(u'alignment/syncmaps/' + address + '.json')
-
-        # align text to audio
-        task = Task(config_string=config)
-        task.text_file_path_absolute = text
-        task.audio_file_path_absolute = audio
-        task.sync_map_file_path_absolute = syncmap
-        ExecuteTask(task).execute()
-        task.output_sync_map_file()
-
-
-def update_database_with_alignments():
-    for address, youtube_id in text_audio_pairs.iteritems():
-        doc = get_doc(title=address)
-        doc.youtube_id = youtube_id
-
-        syncmap = os.path.abspath(u'alignment/syncmaps/' + address + '.json')
-        syncmap = json.load(open(syncmap))['fragments']
-
-        for sent, sync in zip(doc.sentences, syncmap):
-            sent.youtube_id = youtube_id
-            sent.aeneas_start = float(sync['begin'])
-            sent.aeneas_end = float(sync['end'])
-
-            try:
-                sent.manual_start = alignment_fixes[address][sent.index][0]
-
-            except KeyError:
-                pass
-
-            try:
-                sent.manual_end = alignment_fixes[address][sent.index][1]
-
-            except KeyError:
-                pass
-
+    try:
+        for sent_idx, (start, end) in alignment_fixes[address_id]:
+            sent = doc.sentences.filter_by(doc=doc.id, index=sent_idx).one()
+            sent.manual_start = start
+            sent.manual_end = end
             db.session.add(sent)
 
-        db.session.add(doc)
         db.session.commit()
 
+    except KeyError:
+        pass
+
+
+def fix_clinton_1993():  # womp womp
+    '''Fix alignment with YouTube video Qszv668rN20.'''
+    starts_early = set([
+        8, 19, 50, 56, 71, 73, 74, 81, 92, 96, 99, 104, 106, 107, 108,
+        ])
+
+    ends_early = set([8, 36])
+
+    # midly so: 4, 12, 14, 15, 35, 61, 68, 92
+    ends_late = set([
+        1, 2, 3, 5, 6, 10, 11, 13, 17, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
+        29, 30, 31, 32, 33, 34, 37, 41, 42, 43, 44, 45, 46, 47, 48, 51, 56, 60,
+        63, 64, 66, 67, 75, 76, 77, 78, 79, 81, 82, 83, 84, 85, 86, 87, 88,
+        89, 90, 94, 97, 99, 100, 101, 102, 104, 108,
+        ])
+
+    doc = get_doc(title='1993-Clinton1')
+
+    for sent in doc.sentences:
+        try:
+            start, end = alignment_fixes['1993-Clinton1'][sent.index]
+            sent.manual_start = start
+            sent.manual_end = end
+
+        except KeyError:
+            # reset the values
+            sent.manual_start = None
+            sent.manual_end = None
+
+            if sent.index in starts_early:
+                sent.manual_start = sent.aeneas_start + 1
+
+            if sent.index in ends_early:
+                sent.manual_end = sent.aeneas_end + 1
+
+            elif sent.index in ends_late:
+                sent.manual_end = sent.aeneas_end - 1
+
+        db.session.add(sent)
+
+    db.session.commit()
+
+
+# bulk operations -------------------------------------------------------------
+
+def align_and_update_all():
+    '''Align and update all of the addresses according to text_audio_pairs.
+
+    For each address in text_audio_pairs, this function will:
+        - Align the text to the audio in the given YouTube video
+        - Update the address in the database with the aeneas timestamps
+    '''
+    for address_id, youtube_id in text_audio_pairs.iteritems():
+        align(address_id, youtube_id)
+        update(address_id, youtube_id)
+
+
+def fix_all():
+    '''Manually fix all of the alignments according to alignment_fixes.'''
+    for address_id in alignment_fixes.keys():
+        fix(address_id)
+
+# -----------------------------------------------------------------------------
+
+
 if __name__ == '__main__':
-    # get_alignments()
-    update_database_with_alignments()
+    # # in one fell swoop...
+    # align_and_update_all()
+    # fix_all()
+
+    # Bush 1989
+    align('1989-Bush')
+    update('1989-Bush')
+
+    # Clinton 1993
+    align('1993-Clinton1')
+    update('1993-Clinton1')
+    fix_clinton_1993()
+
+    # Clinton 1997
+    align('1997-Clinton2')
+    update('1997-Clinton2')
